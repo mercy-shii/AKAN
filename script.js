be29f53c6992e6caad23552d09532dbf9b46bda8
mercy-shii <wemercy525@gmail.com>
2020-02-17 18:24:26 +0300

    Akan

--- script.js
+++ script.js
@@ -10,17 +10,21 @@ function AkanName() {
 
     }
     
-    var male = document.getElementById("male");
-    var female = document.getElementById("female");
+    var male = document.getElementById("male").value;
+    var female = document.getElementById("female").value;
     var weekdays = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Sturday"];
     var maleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
     var femaleNames = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
     var Dayoftheweek =parseInt( ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7);
     if (female === "female" ){
        document.getElementById("name").innerHTML=femaleNames[Dayoftheweek];
+       document.getElementById("day").innerHTML=weekdays[Dayoftheweek];
+       
     }
-    if (male === "male"){
+    
+    else if (male === "male"){
         document.getElementById("name").innerHTML=maleNames[Dayoftheweek];
+        document.getElementById("day").innerHTML=weekdays[Dayoftheweek];
 
     }
     
